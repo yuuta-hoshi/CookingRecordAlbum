c123b2f147259af7da2958b5c35ef23567ee0daf
yuuta-hoshi <h_y0608@icloud.com>
2023-03-09 16:42:19 +0900

    Initial Commit

---
+++ CookingRecordAlbum/CookingRecordAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingRecordAlbumApp.swift
+//  CookingRecordAlbum
+//
+//  Created by 星優大 on 2023/03/09.
+//
+
+import SwiftUI
+
+@main
+struct CookingRecordAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
